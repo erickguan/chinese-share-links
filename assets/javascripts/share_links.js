3c675bdf8afe5e1523030cd629bc13f02b8baa51
Erick Guan <fantasticfears@gmail.com>
2014-11-06 23:00:31 +0800

    add renren & weibo

---
+++ assets/javascripts/share_links.js
@@ -0,0 +1,20 @@
+(function() {
+  Discourse.ShareLink.addTarget('weibo', {
+    iconClass: 'fa-weibo',
+    generateUrl: function(link, title) {
+      return ("http://service.weibo.com/share/share.php?url=" + encodeURIComponent(link) + "&title=" + encodeURIComponent(title));
+    },
+    shouldOpenInPopup: true,
+    popupHeight: 370
+  });
+
+  Discourse.ShareLink.addTarget('renren', {
+    iconClass: 'fa-renren',
+    generateUrl: function(link, title) {
+      return ("http://widget.renren.com/dialog/share?resourceUrl=" + encodeURIComponent(link) + "&title=" + encodeURIComponent(title) + "&description=" + encodeURIComponent(title));
+    },
+    shouldOpenInPopup: true,
+    popupHeight: 628
+  });
+
+})();
---
+++ plugin.rb
@@ -0,0 +1,6 @@
+# name: Share links
+# about: Add more services to share on.
+# version: 0.1.0
+# author: Erick Guan
+
+register_asset('javascripts/share_links.js')
