f250fe3fdc98f13557f10506450252a60531839c
Erick Guan <fantasticfears@gmail.com>
2014-12-27 23:19:28 +0800

    add qzone and adopt new api

--- assets/javascripts/share_links.js
+++ assets/javascripts/share_links.js
@@ -1,6 +1,6 @@
 (function() {
   Discourse.ShareLink.addTarget('weibo', {
-    iconClass: 'fa-weibo',
+    serviceIcon: '<i class="fa fa-weibo"></i>',
     generateUrl: function(link, title) {
       return ("http://service.weibo.com/share/share.php?url=" + encodeURIComponent(link) + "&title=" + encodeURIComponent(title));
     },
@@ -9,12 +9,21 @@
   });
 
   Discourse.ShareLink.addTarget('renren', {
-    iconClass: 'fa-renren',
+    serviceIcon: '<i class="fa fa-renren"></i>',
     generateUrl: function(link, title) {
       return ("http://widget.renren.com/dialog/share?resourceUrl=" + encodeURIComponent(link) + "&title=" + encodeURIComponent(title) + "&description=" + encodeURIComponent(title));
     },
     shouldOpenInPopup: true,
     popupHeight: 628
+  });
+
+  Discourse.ShareLink.addTarget('qzone', {
+    serviceIcon: '<i class="fa fa-qq"></i>',
+    generateUrl: function(link, title) {
+      return ("http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?url=" + encodeURIComponent(link) + "&desc=" + encodeURIComponent(title) + "&title=" + encodeURIComponent(title) + "&site=" + encodeURIComponent(link));
+    },
+    shouldOpenInPopup: true,
+    popupHeight: 500
   });
 
 })();
