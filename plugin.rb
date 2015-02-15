245a41303500093a2b8cd6a9ac1531cb747f290d
Erick Guan <fantasticfears@gmail.com>
2015-02-15 12:42:31 +0800

    update plugin information

--- plugin.rb
+++ plugin.rb
@@ -1,5 +1,5 @@
 # name: Share links
 # about: Add more services to share on.
-# version: 0.1.1
-# author: Erick Guan
-# rewrite by suanfazu.com
+# version: 0.1.2
+# authors: Erick Guan, suanfazu.com
+# url: https://github.com/discourse/discourse-tagging
